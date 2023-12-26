8f575fe35baa6f051193b7d4b9728c75f11aa844
AK3847 <abdulkadir.co21d2@scet.ac.in>
2023-12-26 15:18:01 +0530

    Revert "Merge commit '3256af8e288dcf7282cd3a4c873d5580b1e68416'"
    This reverts commit 8d2ac644a6397da33aa42e31cfbb2f627e1eb77e, reversing
    changes made to 07dcefac3052a876ef8558e5d0405a4911474d59.

--- sketch.js
+++ sketch.js
@@ -5,23 +5,17 @@ let traillength = 50;
 var parts, nsc, tl, ang;
 let mxtraillength = 500;
 let angle;
-let randomcolor = true;
-let speedMultiplier = 0.2;
-let colors = [
-  [0, 173, 181],
-  [184, 59, 94],
-  [184, 59, 94],
-  [254, 252, 243],
-];
-let play = 1;
+let randomcolor=false;
+let seed=500;
+let speedMultiplier=1;
+let colors=[[0, 173, 181],[184, 59, 94],[184, 59, 94],[254, 252, 243]]
 function setup() {
   frameRate(60);
-  fill(255,255,255);
   createCanvas(windowWidth, windowHeight * 0.66);
   for (let i = 0; i < partnum; i++) {
     particles.push(createVector(random(width), random(height)));
   }
-  //   textFont(font);
+//   textFont(font);    
   button = createButton("Number of Particles");
   parts = createSlider(25, 5000, 2500);
   createP("");
@@ -29,61 +23,58 @@ function setup() {
   nsc = createSlider(100, 2000, 500, 100);
   createP("");
   button = createButton("Trail Length");
-  tl = createSlider(10, 200, 80, 10);
+  tl = createSlider(10, 200, 50, 10);
   createP("");
   button = createButton("Angle");
   ang = createSlider(0, 360, 60);
+  createP("");
+  button = createButton("Noise Strength");
+  sd = createSlider(0, 1000, 100);
+  createP("");
   angle = TWO_PI;
-  button = createButton("Speed");
-  speed = createSlider(speedMultiplier, 10*speedMultiplier, speedMultiplier*2,speedMultiplier/10);
-  angle = TWO_PI;
+  stroke(255);
 }
 function draw() {
-  if (play) {
-    partnum = parts.value();
-    // if (traillength != 100 - t1.value) noisescale = nsc.value();
-    traillength = 100-tl.value();
-    angle = ang.value();
-    noisescale = nsc.value();
-    speedMultiplier=speed.value();
-    background(0, traillength);
-    console.log(traillength);
-    colorMode(RGB);
-    stroke(225,209,227);
-    for (let i = 0; i < partnum; i++) {
+  partnum = parts.value();
+  traillength = 100 - tl.value();
+  angle = ang.value();
+  noisescale = nsc.value();
+  seed=sd.value();
+  background(0, traillength);
+  for (let i = 0; i < partnum; i++) {
       let p = particles[i];
       let particlecolor = colors[int(random(colors.length))];
-    //   let n = noise(p.x / noisescale, p.y / noisescale);
-    let n=noise(p.x*0.003,p.y*0.003);
-    //   let a = angle * n;
-    let a=n*4*PI;
+      let n = noise(p.x / noisescale, p.y / noisescale);
+      let a = angle * n;
       point(p.x, p.y);
       p.x += cos(a) * speedMultiplier;
       p.y += sin(a) * speedMultiplier;
-    //   if (!screenchk(p)) {
-    //     p.x = random(width);
-    //     p.y = random(height);
-    //   }
+      if (!screenchk(p)) {
+        p.x = random(width);
+        p.y = random(height);
+      }
     //   stroke(0);
     }
   }
-}
-function randomfill(Randomfill) {
-  var r, g, b;
-  if (Randomfill) {
-    r = map(sin(angle), -1, 1, 100, 200);
-    g = map(sin(angle), -1, 1, 100, 200);
-    b = map(sin(angle), -1, 1, 100, 200);
-  }
-  (r = 0), (g = 0), (b = 0);
-  return [r, g, b];
+function randomfill(Randomfill)
+{
+    var r,g,b;
+    if(Randomfill){
+        r=map(sin(angle),-1,1,100,200);
+        g=map(sin(angle),-1,1,100,200);
+        b=map(sin(angle),-1,1,100,200);
+    }
+    r=0,g=0,b=0;
+    return [r,g,b];
 }
 function screenchk(v) {
   return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
 }
 // noiseSeed(randomSeed(seed));
-function mouseClicked() {
-  if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
-    noiseSeed(randomSeed(millis));
-  }
-}
+// function mouseClicked()
+// {
+//     // if(mouseX>=0 && mouseX<=width && mouseY>=0 && mousey<=height)
+//     // {
+//     // }
+//     noiseSeed(randomSeed(seed));
+// }
