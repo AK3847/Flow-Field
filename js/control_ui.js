0881cc878d32de4d210e89dff912c9582a38496b
AK3847 <abdulkadir.co21d2@scet.ac.in>
2023-12-28 11:50:51 +0530

    js  files format change

--- js/control_ui.js
+++ js/control_ui.js
@@ -1,35 +1,43 @@
-function Slider(label,min=0,max=50,val=10,step=1,parent=createDiv(),update=()=>{})
-{
-    let wrap=createDiv(label);
-    wrap.parent(parent);
-    wrap.class("slider");
-    let slider=createSlider(min,max,val,step);
-    slider.input(update);
-    slider.class("form-control-range");
-    slider.parent(wrap);
-    return (slider);
+function Slider(
+  label,
+  min = 0,
+  max = 100,
+  val = 10,
+  step = 10,
+  parent = createDiv(),
+  update = () => {}
+) {
+  let wrap = createDiv(label);
+  wrap.parent(parent);
+  wrap.class("slider");
+  let slider = createSlider(min, max, val, step);
+  slider.input(update);
+  slider.class("form-control-range");
+  slider.parent(wrap);
+  return slider;
 }
 
-function Button(text,parent,callback,type="not_modal")
-{
-    let buttonwrap=createDiv();
-    buttonwrap.class("button-wrapper");
-    let button=createButton(text);
-    button.class("butn");
-    button.parent(buttonwrap);
-    buttonwrap.parent(parent);
-    button.mousePressed(callback);
+function Button(text, parent, callback, type = "not_modal") {
+  let buttonwrap = createDiv();
+  buttonwrap.class("button-wrapper");
+  buttonwrap.parent(parent);
+  let button = createButton(text);
+  button.class("butn");
+  button.parent(buttonwrap);
+  button.mousePressed(callback);
 }
 
-
-function Colorpicker(label="Pick a color",startcolor="blue",parent=createDiv(),update=()=>{})
-{
-    let wrap=createDiv(label);
-    wrap.class("color-picker");
-    wrap.parent(parent);
-
-    let picker=createColorPicker(startcolor);
-    picker.input(()=>update(picker.value()));
-    picker.parent(wrap);
-    return (picker);
+function Colorpicker(
+  label = "Pick a color",
+  startcolor = "blue",
+  parent = createDiv(),
+  update = () => {}
+) {
+  let wrap = createDiv(label);
+  wrap.class("color-picker");
+  wrap.parent(parent);
+  let picker = createColorPicker(startcolor);
+  picker.input(() => update(picker.value()));
+  picker.parent(wrap);
+  return picker;
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,55 +1,116 @@
-let X_START=0;
-const Y_START=0;
-let xoff=0,yoff=0,zoff=0;
-let particles=[],flowfield=[];
+let X_START = 0;
+const Y_START = 0;
+let xoff = 0,
+  yoff = 0,
+  zoff = 0;
+let particles = [],
+  flowfield = [];
 let canvas;
 let nrow, ncol, rectWidth, rectHeight;
-let xIncre, yIncre, zIncre, partSlide, opacitySlide, strokeCP, backgroundColorPicker;
+let xIncre, yIncre, zIncre;
+let partSlide, opacitySlide, strokeCP, backgroundColorPicker;
 
-function makeControls()
-{
-
+function makeControls() {
   let controlWrap = createDiv().id("control-wrap");
   let controlHead = createDiv("<p>Tools</p>");
   controlHead.parent(controlWrap);
-  partSlide = Slider("<span>Number of Particles</span>", minVal = 10, maxVal = 10000, value = 1000, step = 10, parent = controlWrap, clearCanvas);
-  zIncre = Slider("<span>Change in Force</span>", minVal = 0, maxVal = .3, value = .01, step = .0001, parent = controlWrap, clearCanvas);
-  nrowSlider = Slider("<span>Vertical Drift</span>", minVal = 2, maxVal = 50, value = 30, step = 1, parent = controlWrap, clearCanvas);
-  ncolSlider = Slider("<span>Horizontal Drift</span>", minVal = 2, maxVal = 50, value = 30, step = 1, parent = controlWrap, clearCanvas);
-  xIncre = Slider("<span>Horizontal Flow Intensity</span>", minVal = .0001, maxVal = .3, value = .05, step = .0001, parent = controlWrap, clearCanvas);
-  yIncre = Slider("<span>Vertical Flow Intenstiy</span>", minVal = .0001, maxVal = .3, value = .05, step = .0001, parent = controlWrap, clearCanvas);
-  opacitySlide = Slider("<span>Line Opacity</span>", minVal = 0, maxVal = 1, value = .1, step = .01, parent = controlWrap);
-  strokeCP = Colorpicker("<span>Line Color</span>", startColor = "rgb(96, 158, 162)", parent = controlWrap);
+  partSlide = Slider(
+    "<span>Number of Particles</span>",
+    (minVal = 100),
+    (maxVal = 8000),
+    (value = 800),
+    (step = 50),
+    (parent = controlWrap),
+    clearCanvas
+  );
+  zIncre = Slider(
+    "<span>Change in Force</span>",
+    (minVal = 0),
+    (maxVal = 0.3),
+    (value = 0.01),
+    (step = 0.0001),
+    (parent = controlWrap),
+    clearCanvas
+  );
+  nrowSlider = Slider(
+    "<span>Vertical Drift</span>",
+    (minVal = 2),
+    (maxVal = 50),
+    (value = 30),
+    (step = 1),
+    (parent = controlWrap),
+    clearCanvas
+  );
+  ncolSlider = Slider(
+    "<span>Horizontal Drift</span>",
+    (minVal = 2),
+    (maxVal = 50),
+    (value = 30),
+    (step = 1),
+    (parent = controlWrap),
+    clearCanvas
+  );
+  xIncre = Slider(
+    "<span>Horizontal Flow Intensity</span>",
+    (minVal = 0.0001),
+    (maxVal = 0.3),
+    (value = 0.05),
+    (step = 0.0001),
+    (parent = controlWrap),
+    clearCanvas
+  );
+  yIncre = Slider(
+    "<span>Vertical Flow Intenstiy</span>",
+    (minVal = 0.0001),
+    (maxVal = 0.3),
+    (value = 0.05),
+    (step = 0.0001),
+    (parent = controlWrap),
+    clearCanvas
+  );
+  opacitySlide = Slider(
+    "<span>Line Opacity</span>",
+    (minVal = 0),
+    (maxVal = 1),
+    (value = 0.1),
+    (step = 0.01),
+    (parent = controlWrap)
+  );
+  strokeCP = Colorpicker(
+    "<span>Line Color</span>",
+    (startColor = "rgb(96, 158, 162)"),
+    (parent = controlWrap)
+  );
   // backgroundColorPicker = Colorpicker("<span>Background Color</span>", startColor = "black", parent = controlWrap, (d) => setBackgroundColor(d));
 
   // Buttons
   Button("&nbsp;&nbsp;Pause&nbsp;&nbsp;", controlWrap, noLoop);
   Button("&nbsp;Resume", controlWrap, loop);
-  Button("&nbsp;&nbsp;&nbsp;Clear&nbsp;&nbsp;&nbsp;&nbsp;", controlWrap, clearCanvas);
+  Button(
+    "&nbsp;&nbsp;&nbsp;Clear&nbsp;&nbsp;&nbsp;&nbsp;",
+    controlWrap,
+    clearCanvas
+  );
   Button("Download", controlWrap, download);
   Button("&nbsp;&nbsp;GitHub&nbsp;&nbsp;", controlWrap, () => {
     window.open("https://github.com/AK3847/Flow-Field", "_blank");
   });
   return controlWrap;
-
 }
 
 function download() {
   noLoop();
-  let link = document.createElement('a');
-  link.download = 'flow_field.png';
-  link.href = document.querySelector('canvas').toDataURL()
+  let link = document.createElement("a");
+  link.download = "flow_field.png";
+  link.href = document.querySelector("canvas").toDataURL();
   link.click();
   loop();
 }
 
-
-
 function setBackgroundColor() {
   // canvas.style("background-color", backgroundColorPicker.value())
-  canvas.style("background-color","#000000");
+  canvas.style("background-color", "#000000");
 }
-
 
 function createEmptyParticles() {
   particles = [];
@@ -59,29 +120,30 @@ function createEmptyParticles() {
 }
 
 function clearCanvas() {
-  clear();  
+  clear();
   createEmptyParticles();
-  flowfield = [];  
+  flowfield = [];
   xoff = X_START = random(100);
   yoff = random(100);
   zoff = random(100);
 }
 
-function setup()
-{
-    let container=createDiv().id("main-container");
-    let controls=makeControls();
-    controls.parent(container);
-    let canvasContainer=createDiv();
-    canvas=createCanvas((windowWidth)*0.85,(windowHeight-120)*0.8).id("canvas-container");
-    setBackgroundColor();
-    canvas.parent(canvasContainer);
-    canvasContainer.parent(container);
+function setup() {
+  let container = createDiv().id("main-container");
+  let controls = makeControls();
+  controls.parent(container);
+  let canvasContainer = createDiv();
+  canvas = createCanvas(windowWidth * 0.85, (windowHeight - 120) * 0.8).id(
+    "canvas-container"
+  );
+  setBackgroundColor();
+  canvas.parent(canvasContainer);
+  canvasContainer.parent(container);
 
-    colorMode(RGB,100);
+  colorMode(RGB, 255);
 
-    getSize();
-    createEmptyParticles();
+  getSize();
+  createEmptyParticles();
 }
 
 function getSize() {
@@ -91,13 +153,13 @@ function getSize() {
   rectHeight = height / nrow;
 }
 
-function draw() {  
-  getSize();  
+function draw() {
+  getSize();
   for (let row = 0; row < nrow; row++) {
     for (let col = 0; col < ncol; col++) {
-      let angle = noise(xoff, yoff, zoff) * 2 * PI;
+      let angle = noise(xoff, yoff, zoff) * 4 * PI;
       var v = p5.Vector.fromAngle(angle);
-      v.setMag(1);      
+      v.setMag(1);
       flowfield.push([v.x, v.y]);
       xoff += xIncre.value();
     }
@@ -110,5 +172,5 @@ function draw() {
     particles[i].edges();
     particles[i].show();
   }
-  zoff += zIncre.value()*2; 
+  zoff += zIncre.value() * 2;
 }
